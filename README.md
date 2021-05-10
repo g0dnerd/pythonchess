61c59d32d15c03d39dee13444780de1bda8ada4b
paul <kukowskipaul@gmail.com>
2021-05-10 16:54:17 -0400

    added basic pawn moves, added .gitignore, misc

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # pythonchess
+
+## TODOs
+
+BASIC GAME FUNCTIONS
+* function to check move legality (legal target square, discoveries, moving into checks, castling)
+* use proper error handling to handle move legality
+* material score
+* en passant, castling, promoting
+* recognize checks and checkmates
+  
+ARTIFICAL INTELLIGENCE
+* ...
+
--- game.py
+++ game.py
@@ -1,6 +1,3 @@
-import numpy as np
-import config
-
 class boardState:
 
 	board = list()
@@ -25,6 +22,7 @@ class boardState:
 
 
 	def print_board(self):
+		# prints the board upside down since it's stored in a list starting at a1
 		print("  a b c d e f g h")
 		for i in range(8):
 			print(8 - i, end = " ")
@@ -38,15 +36,15 @@ class boardState:
 
 	def make_move(self, userMove):
 		# TODO: Decoding Logic for algebraic notation
-		# TODO: check move legality
-		print("Making move " + userMove)
 
+		print("Making move " + userMove)
 		# if move length is = 2, it's a non-capturing pawn move
 		if len(userMove) == 2:
-			# TODO
 			moveToRow = int(userMove[1])
-			moveToRow-=1
+			moveToRow -= 1
+			# uses the target square's ASCII value to determine the File to move to
 			moveToFile = ord(userMove[0]) - 97
+
 			if self.board[moveToRow+self.toMove][moveToFile].lower() == 'p':
 				self.board[moveToRow+self.toMove][moveToFile] = 'x'
 
@@ -63,13 +61,17 @@ class boardState:
 				else:
 					self.board[moveToRow][moveToFile] = 'p'
 
+		# pass the turn to the other player
 		if self.toMove == -1:
 			self.toMove = 1
 		else:
 			self.toMove = -1
 
+		self.moveCount += 1
 		self.print_board()
 
+	# def is_legal(self, move):
+		# TODO: legality detection
 
 	# def is_check(self):
 		# TODO: Logic for check detection
@@ -79,15 +81,16 @@ def main():
 	print("Welcome to pychess.")
 	print("You are playing as WHITE")
 	print("")
-	config.moveCount = 0
 	gameState = boardState(True, True, -1)
-	print(gameState.toMove)
+	gameState.moveCount = 0
 	gameState.print_board()
 	for i in range(5):
+		print("Move " + str(gameState.moveCount + 1))
 		userInput = input("Please enter your move: ")
 		gameState.make_move(userInput)
 
 
 if __name__ == "__main__":
 	main()
+
 
