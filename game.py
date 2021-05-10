6f2480d38eb3046cf357bb3c16dcbef19ec66fce
paul <kukowskipaul@gmail.com>
2021-05-10 12:03:13 -0400

    added basic pawn moves

--- game.py
+++ game.py
@@ -7,35 +7,72 @@ class boardState:
 	moveCount = 0
 
 	board = [
-		['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
-		['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-		['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-		['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-		['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
-		['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
+		['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
 		['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-		['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']]
+		['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
+		['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
+		['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
+		['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
+		['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+		['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r']]
 
 
-	def __init__(self, whiteCanCastle, blackCanCastle):
+	def __init__(self, whiteCanCastle, blackCanCastle, toMove):
 		self.whiteCanCastle = whiteCanCastle
 		self.blackCanCastle = blackCanCastle
+		#  white to move = -1, black to move = 1
+		self.toMove = toMove
 
 
-	def printBoard(self):
-		print("  A B C D E F G H")
+	def print_board(self):
+		print("  a b c d e f g h")
 		for i in range(8):
 			print(8 - i, end = " ")
 			for j in range(8):
 				if j < 7:
-					print(self.board[i][j], end = " ")
+					print(self.board[7-i][j], end = " ")
 				else:
-					print(self.board[i][j])
+					print(self.board[7-i][j])
+		print("")
 
 
-	def make_move(self, inputString):
+	def make_move(self, userMove):
 		# TODO: Decoding Logic for algebraic notation
-		print("Move " + str(moveCount) + ":")
+		# TODO: check move legality
+		print("Making move " + userMove)
+
+		# if move length is = 2, it's a non-capturing pawn move
+		if len(userMove) == 2:
+			# TODO
+			moveToRow = int(userMove[1])
+			moveToRow-=1
+			moveToFile = ord(userMove[0]) - 97
+			if self.board[moveToRow+self.toMove][moveToFile].lower() == 'p':
+				self.board[moveToRow+self.toMove][moveToFile] = 'x'
+
+				if self.toMove == -1:
+					self.board[moveToRow][moveToFile] = 'P'
+				else:
+					self.board[moveToRow][moveToFile] = 'p'
+
+			elif self.board[moveToRow+(self.toMove*2)][moveToFile].lower() == 'p':
+				self.board[moveToRow+(self.toMove*2)][moveToFile] = 'x'
+
+				if self.toMove == -1:
+					self.board[moveToRow][moveToFile] = 'P'
+				else:
+					self.board[moveToRow][moveToFile] = 'p'
+
+		if self.toMove == -1:
+			self.toMove = 1
+		else:
+			self.toMove = -1
+
+		self.print_board()
+
+
+	# def is_check(self):
+		# TODO: Logic for check detection
 
 
 def main():
@@ -43,8 +80,13 @@ def main():
 	print("You are playing as WHITE")
 	print("")
 	config.moveCount = 0
-	gameState = boardState(True, True)
-	gameState.printBoard()
+	gameState = boardState(True, True, -1)
+	print(gameState.toMove)
+	gameState.print_board()
+	for i in range(5):
+		userInput = input("Please enter your move: ")
+		gameState.make_move(userInput)
+
 
 if __name__ == "__main__":
 	main()
